bdc327b45dcce45cd846a50aff4d54d2fcf3b833
GustavoEliseu <30469845+GustavoEliseu@users.noreply.github.com>
2017-10-26 11:38:26 -0300

    Initial commit

---
+++ app/src/main/java/gustavo/com/recicleviewgustavo/AdaptadorRecycler.java
@@ -0,0 +1,45 @@
+package gustavo.com.recicleviewgustavo;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by aluno on 26/10/17.
+ */
+
+public class AdaptadorRecycler extends RecyclerView.Adapter<AdaptadorRecycler.ViewHolder> {
+    private String[] mDataset;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+        public ViewHolder(TextView v) {
+            super(v);
+            mTextView =v;
+        }
+    }
+    public AdaptadorRecycler(String[] myDataset) {
+        mDataset = myDataset;
+    }
+    @Override
+    public AdaptadorRecycler.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.my_text_view, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(AdaptadorRecycler.ViewHolder holder, int position) {
+        holder.mTextView.setText(mDataset[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
